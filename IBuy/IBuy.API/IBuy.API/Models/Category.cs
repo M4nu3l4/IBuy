09128cffeba5a3fef936be7fbe2f3ec03f82b71a
Manuela Lissia <142220899+M4nu3l4@users.noreply.github.com>
2026-07-16 10:57:21 +0200

    Clean up comments in Category model
    Removed commented line about one-to-many relationship.

--- IBuy/IBuy.API/IBuy.API/Models/Category.cs
+++ IBuy/IBuy.API/IBuy.API/Models/Category.cs
@@ -9,7 +9,7 @@ namespace IBuy.API.Models
         public string Name { get; set; }
         public string? Description { get; set; }
 
-        // Relazione uno-a-molti: una categoria può avere più prodotti
+        
         public ICollection<Product>? Products { get; set; }
     }
 }
