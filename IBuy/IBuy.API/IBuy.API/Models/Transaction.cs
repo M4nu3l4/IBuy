150321f3aab077c3ae7f6e973c9b73544cee6301
Manuela Lissia <142220899+M4nu3l4@users.noreply.github.com>
2026-07-16 10:57:58 +0200

    Remove comments from Transaction model properties

--- IBuy/IBuy.API/IBuy.API/Models/Transaction.cs
+++ IBuy/IBuy.API/IBuy.API/Models/Transaction.cs
@@ -13,7 +13,7 @@ public class Transaction
     public int Quantity { get; set; }
     public DateTime Date { get; set; }
     public string OrderStatus { get; set; }
-    public string? RequestedAction { get; set; } // Added this property
-    public string? CustomerNote { get; set; } // Added this property
-    public bool IsPendingApproval { get; set; } // Added this property
+    public string? RequestedAction { get; set; } 
+    public string? CustomerNote { get; set; } 
+    public bool IsPendingApproval { get; set; } 
 }
